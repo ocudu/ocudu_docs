1a73b07e45004beb4f779c3f7fc3f0db6af50946
Alfredo Sáez <alfredo.saez@srs.io>
2026-01-09 17:33:02 +0100

    docusaurus: basic config

---
+++ docusaurus.config.js
@@ -0,0 +1,127 @@
+/*
+ *
+ * Copyright 2021-2026 Software Radio Systems Limited
+ *
+ * By using this file, you agree to the terms and conditions set
+ * forth in the LICENSE file which can be found at the top level of
+ * the distribution.
+ *
+ */
+
+const { themes } = require('prism-react-renderer');
+
+const gitlab_namespace = 'ocudu';
+const gitlab_project = 'ocudu';
+const url = `https://${gitlab_namespace}.gitlab.io/`;
+const baseUrl = process.env.BASE_URL || '/';
+const gitlab_repo_url = `https://gitlab.com/${gitlab_namespace}`;
+
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+module.exports = {
+  title: 'OCUDU',
+  tagline: 'Open Centralized Unit Distributed Unit',
+  url: url,
+  baseUrl: baseUrl,
+  onBrokenLinks: 'throw',
+  favicon: 'https://srs.io/wp-content/uploads/ocudu_color.png',
+  organizationName: gitlab_namespace,
+  projectName: gitlab_project,
+  markdown: {
+    mermaid: true,
+    hooks: {
+      onBrokenMarkdownLinks: 'warn',
+    },
+  },
+  themes: ['@docusaurus/theme-mermaid'],
+  themeConfig: {
+    prism: {
+      theme: themes.github,
+      darkTheme: themes.dracula,
+      defaultLanguage: 'bash',
+      additionalLanguages: ['bash', 'shell-session', 'cmake', 'cpp', 'json', 'yaml'],
+    },
+    navbar: {
+      title: '',
+      logo: {
+        alt: 'OCUDU Logo',
+        src: 'https://srs.io/wp-content/uploads/ocudu_color.png',
+      },
+      items: [
+        {
+          to: '/',
+          position: 'left',
+          label: 'Documentation',
+        },
+        {
+          href: 'https://ocudu.org',
+          label: 'Website',
+          position: 'right',
+        },
+        {
+          href: gitlab_repo_url,
+          label: 'Gitlab',
+          position: 'right',
+        },
+      ],
+    },
+    announcementBar: {
+      id: 'wip',
+      content:
+        'This documentation is a work in progress!',
+      backgroundColor: '#ddc36fff',
+      textColor: '#091E42',
+      isCloseable: false,
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'Website',
+              href: 'https://ocudu.org'
+            },
+            {
+              label: 'Gitlab',
+              href: gitlab_repo_url,
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Software Radio Systems.`,
+    },
+  },
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          path: 'docs',
+          routeBasePath: '/',
+          sidebarPath: require.resolve('./sidebars.js'),
+        },
+        blog: false,
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }
+    ],
+  ],
+  plugins: [
+    [
+      require.resolve("@easyops-cn/docusaurus-search-local"),
+      {
+        hashed: true,
+        indexDocs: true,
+        indexPages: true,
+        indexBlog: false,
+        docsRouteBasePath: '/',
+      },
+    ],
+  ]
+};
