7a360513189269eef6cbefd2810655073bc7735f
Niklas Moser <niklas.moser@srs.io>
2026-02-17 12:27:17 +0100

    docusaurus: add footnote commits to reflect the repositories used to build the docu
    Signed-off-by: Niklas Moser <niklas.moser@srs.io>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,6 +8,7 @@
  *
  */
 
+const { execSync } = require('child_process');
 const { themes } = require('prism-react-renderer');
 
 const gitlab_namespace = 'ocudu';
@@ -15,6 +16,38 @@ const gitlab_project = 'ocudu';
 const url = `https://${gitlab_namespace}.gitlab.io/`;
 const baseUrl = process.env.BASE_URL || '/';
 const gitlab_repo_url = `https://gitlab.com/${gitlab_namespace}`;
+const ocudu_repo_url = `${gitlab_repo_url}/${gitlab_project}`;
+const ocudu_docs_repo_url = `${gitlab_repo_url}/ocudu_docs`;
+const ocudu_oran_apps_repo_url = `${gitlab_repo_url}/ocudu_elements/ocudu_oran_apps`;
+const ocudu_netconf_repo_url = `${ocudu_oran_apps_repo_url}/ocudu_netconf`;
+const ocudu_o1_adapter_repo_url = `${ocudu_oran_apps_repo_url}/ocudu_o1_adapter`;
+
+// Returns Commit Sha of given repository
+function getRepoCommitSha(repoPath, repoName) {
+  try {
+    return execSync(`git -C ${repoPath} rev-parse HEAD`, { stdio: ['ignore', 'pipe', 'ignore'] })
+      .toString()
+      .trim();
+  } catch (error) {
+    console.error(`[commit-info] ERROR: Could not resolve commit SHA for ${repoName} in path '${repoPath}'`);
+    return '';
+  }
+}
+
+// Shortens given Commit Sha taking only the first 8 letters for better readability
+function getShortSha(sha) {
+  return sha ? sha.slice(0, 8) : 'ERROR - commit SHA not found';
+}
+
+// Get Commit Sha's of used repositories and shorten them for printing in the footnote
+const ocuduCommitSha = getRepoCommitSha('docs/ocudu', 'OCUDU');
+const ocuduDocsCommitSha = getRepoCommitSha('.', 'OCUDU Docs');
+const ocuduNetconfCommitSha = getRepoCommitSha('oran_apps/ocudu_netconf', 'OCUDU Netconf');
+const ocuduO1AdapterCommitSha = getRepoCommitSha('oran_apps/ocudu_o1_adapter', 'OCUDU O1 Adapter');
+const ocuduCommitShort = getShortSha(ocuduCommitSha);
+const ocuduDocsCommitShort = getShortSha(ocuduDocsCommitSha);
+const ocuduNetconfCommitShort = getShortSha(ocuduNetconfCommitSha);
+const ocuduO1AdapterCommitShort = getShortSha(ocuduO1AdapterCommitSha);
 
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
@@ -112,6 +145,27 @@ module.exports = {
             {
               label: 'Gitlab',
               href: gitlab_repo_url,
+            },
+          ],
+        },
+        {
+          title: 'Repositories used for building this website',
+          items: [
+            {
+              label: `OCUDU: ${ocuduCommitShort}`,
+              href: `${ocudu_repo_url}/-/tree/${ocuduCommitSha}`,
+            },
+            {
+              label: `OCUDU Docs: ${ocuduDocsCommitShort}`,
+              href: `${ocudu_docs_repo_url}/-/tree/${ocuduDocsCommitSha}`,
+            },
+            {
+              label: `OCUDU Netconf: ${ocuduNetconfCommitShort}`,
+              href: `${ocudu_netconf_repo_url}/-/tree/${ocuduNetconfCommitSha}`,
+            },
+            {
+              label: `OCUDU O1 Adapter: ${ocuduO1AdapterCommitShort}`,
+              href: `${ocudu_o1_adapter_repo_url}/-/tree/${ocuduO1AdapterCommitSha}`,
             },
           ],
         },
