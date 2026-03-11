2bec16a0049ca262bfb21be0a813a5aaa58b81df
Robert Falkenberg <robert.falkenberg@srs.io>
2026-03-11 08:35:05 +0100

    license: update license headers
    Signed-off-by: Robert Falkenberg <robert.falkenberg@srs.io>

--- docs/user_manual/outputs/assets/metrics_ws_receiver.py
+++ docs/user_manual/outputs/assets/metrics_ws_receiver.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+# SPDX-FileCopyrightText: Copyright (C) 2021-2026 Software Radio Systems Limited
+# SPDX-License-Identifier: BSD-3-Clause-Open-MPI
+
 from contextlib import suppress
 import os
 import json
